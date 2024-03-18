9bf67f9d0c6c62d6047e29b6bd9f6f37fdb2f2dd
kota-aso <kotaaso1212@gmail.com>
2024-03-18 23:11:15 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by Kota Aso on 2024/03/18.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
