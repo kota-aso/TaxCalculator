812b037be1a593f94e4eae99439d834a36dff4cf
kota-aso <kotaaso1212@gmail.com>
2024-03-19 00:12:38 +0900

    complete

--- TaxCalculator/ContentView.swift
+++ TaxCalculator/ContentView.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State var inputText = ""
+    @State var tax8 = 0.0
+    @State var tax10 = 0.0
+
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             TextField("ここに文字を入力",text: $inputText)
+                .keyboardType(.numberPad)
             Button("計算"){
+                tax8 = Double(inputText)! * 0.08
+                tax10 = Double(inputText)! * 0.1
             }
-            Text("価格：")
-            Text("消費税8％：")
-            Text("消費税10％：")
+            Text("価格：\(inputText)")
+            Text("消費税8％：\(tax8)")
+            Text("消費税10％：\(tax10)")
         }
     }
 }
